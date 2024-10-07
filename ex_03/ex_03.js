49533908fb738366f6bc5f8fbc9bb0f9ee8d1130
karell606 <karell.azabe@epitech.digital>
2024-10-07 13:19:29 +0200

    ex_03.js

---
+++ ex_03/ex_03.js
@@ -0,0 +1,26 @@
+function addCarToAgent(agent,brand,model, color, power) {
+		if (typeof agent !== 'object' || brand !== 'string' || typeof model !== 'string' || typeof color !== 'string' || typeof power !== "string" ) {
+		    return null;
+  console.warn("Error: One or more keys are missing or incorrectly typed.")
+ }	
+  
+	agent.car = {
+       	brand: brand,
+        model: model,
+        color: color,
+        power: power
+   };
+
+    return agent;
+}
+
+let agent = { 
+    Firstname: "James",
+    lastname: "Bond"
+};
+
+agent = addCarToAgent(agent,'Aston Martin','DB5','Grey','285bhp') ;
+
+if (agent) {
+	shareThatBeauty(agent);
+}
