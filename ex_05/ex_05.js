740dbc5be65a7fd0510122421a0a0a144c66e362
karell606 <karell.azabe@epitech.digital>
2024-10-17 13:16:19 +0200

    Update ex_05.js

--- ex_05/ex_05.js
+++ ex_05/ex_05.js
@@ -1,32 +1,20 @@
-class Car {
-    constructor(color, brand, model) {
-        this.color = color; 
-        this.brand = brand; 
-        this.model = model; 
-    }
-//Le constructor permet de définir et d’initialiser les propriétés de l’objet dès sa création. Par exemple, dans la classe Car, le constructeur initialise les propriétés color, brand, et model//
- 
+const car = {
+    color: 'grey',
+    model: 'DB5',
+    brand: 'Aston Martin',
     logInfo() {
-        console.log(`Car information: ${this.color}, ${this.model}, ${this.brand}`) ;//La méthode logInfo affiche les informations de la voiture dans la console.//
-//`` ce sont des backsticks qui est utilisés pour entourer la chaîne de caractères. Ils permettent d’utiliser des variables à l’intérieur de la chaîne.//
+      console.log(`Car information: ${this.color}, ${this.model}, ${this.brand}`);
     }
-}
- 
-class Agent {
-    constructor(name, code, age, car) {
-        this.name = name; 
-        this.code = code; 
-        this.age = age; 
-        this.car = car; 
-    }
-  
+  };
+  const agent = {
+    name: 'James Bond',
+    code: '007',
+    age: 57,
+    car: car,
     logInfo() {
-        console.log(`Agent information: ${this.name}, ${this.code}, ${this.age}`) ; //${}insere la valeur de ... (par exemple this color) dans la chaine.//
-        this.car.logInfo(); 
+      console.log(`Agent information: ${this.name}, ${this.code}, ${this.age}`);
+      console.log(`${this.name}'s Car information: ${this.car.color}, ${this.car.model}, ${this.car.brand}`);
     }
-}
- 
-const bondCar = new Car('grey', 'DB5', 'Aston Martin');
-const agent = new Agent('James Bond', '007', 57, bondCar);
- 
-agent.logInfo();
+  };
+  agent.logInfo();
+  agent.car.logInfo();
