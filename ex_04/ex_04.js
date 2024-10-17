64e4d318ff729d56a81c902db2674d4b20fdff30
karell606 <karell.azabe@epitech.digital>
2024-10-17 13:13:33 +0200

    Update ex_04.js

--- ex_04/ex_04.js
+++ ex_04/ex_04.js
@@ -1,23 +1,28 @@
-function updateLicensePlates(agent, plates) {
-    agent.car = { licensePlates: plates }; 
-    return agent; 
-}
- 
+let agent = {
+    firstName: 'James',
+    lastName: 'Bond',
+    car: {
+        brand: 'Aston Martin',
+        model: 'DB5',
+        color: 'Silver',
+        power: '282 hp'
+    }
+};
+function updateLicensePlates(agent, plate) {
+    if (agent.car) {
+        agent.car.licensePlates = plate;
+    }
+    return agent;
+};
 function showLicensePlates(agent) {
-    const ul = document.createElement('ul');
- 
-    agent.car.licensePlates.forEach(plate => {
-        const li = document.createElement('li'); 
-        li.textContent = plate; 
-        ul.appendChild(li);  
-    });
- 
-    document.body.appendChild(ul);
-    //Ajoute l’élément <ul> au corps du document HTML, affichant ainsi la liste des plaques d’immatriculation sur la page.//
+    if (agent.car && agent.car.licensePlates) {
+        let platesContainer = document.querySelector(".plates-container");
+        agent.car.licensePlates.forEach(function(plate) {
+            let li = document.createElement("li");
+            li.textContent = plate;
+            platesContainer.appendChild(li);
+        });
+    }
 }
- 
-let bond = {}; //Crée un objet vide nommé bond//
-let plates = ['LU 6789', '4711-EA-62', 'BMT 216A']; 
-bond = updateLicensePlates(bond, plates); 
-//Appelle la fonction updateLicensePlates avec l’objet bond et le tableau plates, et met à jour bond avec les nouvelles informations.//
-showLicensePlates(bond);
+updateLicensePlates(agent, ['LU 6789', '4711-EA-62', 'BMT 216A']);
+showLicensePlates(agent)
